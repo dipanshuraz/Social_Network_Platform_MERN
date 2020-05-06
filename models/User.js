39e0920c046f10c45816d9e3c782ce9aae0ce5c6
Deepanshu Prajapati <dipanshuraz2@gmail.com>
2020-05-06 21:50:41 +0530

    auth

--- models/User.js
+++ models/User.js
@@ -1,10 +1,27 @@
 const mongoose = require("mongoose");
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required },
-  avatar: { type: String },
-  date: { type: Date, default: Date.now },
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = User = mongoose.model("user", UserSchema);
+module.exports = User = mongoose.model("users", UserSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,73 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
+const User = require("../../models/User");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+
 // @route  POST api/users
 // @desc  Register User route
 // @access Public
-router.post("/", (req, res, next) => {
-  console.log(req.body);
-  res.send("Users Route");
-});
+router.post(
+  "/",
+  [
+    check("name", "name is required").not().isEmpty(),
+    check("email", "include a valid email").isEmail(),
+    check("password", "please enter valid password").isLength({ min: 6 }),
+  ],
+
+  async (req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(400).json({ error: error.array() });
+    }
+    const { name, email, password } = req.body;
+    try {
+      // Check If User exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: [{ msg: "User already exists" }] });
+      }
+
+      // Get User Gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      // Encrypt Password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get("mysecretjwttoken"),
+        {
+          expiresIn: 36000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
